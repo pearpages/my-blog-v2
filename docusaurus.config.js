00ed999814dc00dc80b706a973556f972e3fb438
Pere Pages <pere.pages@ocado.com>
2020-11-14 19:45:23 +0100

    feat: change logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'pages.ninja.com',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
