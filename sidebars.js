9241bf9306d10c2d0b288ac3d363e9302502202c
Pere Pages <pere.pages@ocado.com>
2020-12-05 16:25:41 +0100

    feat: links resources

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,10 @@ module.exports = {
     Features: ['mdx'],
     'External resources': [
       { type: 'link', label: 'Learn git Branching', href: 'https://learngitbranching.js.org/' },
+      { type: 'link', label: 'Learn Storybook', href: 'https://www.learnstorybook.com/' },
       { type: 'link', label: 'UML Class Diagram Tutorial', href: 'https://www.visual-paradigm.com/guide/uml-unified-modeling-language/uml-class-diagram-tutorial/' },
+      { type: 'link', label: 'TypeScript Pro tips/patterns', href: 'https://medium.com/@martin_hotell/10-typescript-pro-tips-patterns-with-or-without-react-5799488d6680' },
+      { type: 'link', label: 'Rtype', href: 'https://github.com/ericelliott/rtype' },
       { type: 'link', label: 'Flow', href: 'https://google.es' },
       { type: 'link', label: 'Immutable.js', href: 'https://google.es' },
       { type: 'link', label: 'Lodash', href: 'https://google.es' },
