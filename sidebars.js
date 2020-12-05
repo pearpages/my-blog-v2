d2fb8465f49893f667c5754f09275cf90bc2bb29
Pere Pages <pere.pages@ocado.com>
2020-12-05 16:48:54 +0100

    feat: js books

--- sidebars.js
+++ sidebars.js
@@ -9,6 +9,7 @@ module.exports = {
       { type: 'link', label: 'Typescript', href: 'https://google.es' },
       { type: 'link', label: 'TypeScript Pro tips/patterns', href: 'https://medium.com/@martin_hotell/10-typescript-pro-tips-patterns-with-or-without-react-5799488d6680' },
       { type: 'link', label: '12 Javascript books', href: 'https://medium.com/javascript-scene/12-books-every-javascript-developer-should-read-9da76157fb3' },
+      { type: 'link', label: 'Top 20 Javascript books', href: 'https://www.blog.consultants500.com/web-mobile-design-and-development/top-20-best-javascript-book-recommended-times-javascript-pros/' },
       { type: 'link', label: 'Flow', href: 'https://google.es' },
       { type: 'link', label: 'Tern.js', href: 'https://google.es' },
       { type: 'link', label: 'repl.it', href: 'https://google.es' },
